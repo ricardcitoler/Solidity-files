87cdb5db393f76d8b7eff15df78d52b9d7e08b76
ricardcitoler <116341156+ricardcitoler@users.noreply.github.com>
2023-04-03 17:57:39 +0200

    Add files via upload

---
+++ prueba1.sol
@@ -0,0 +1,31 @@
+
+//@notice this is a test contract to practice the use of the solidity language
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract TaskList {
+    struct Task {
+        string description;
+        bool completed;
+        address owner;
+    }
+
+    Task[] public tasks;
+
+    function createTask(string memory _description) public {
+        tasks.push(Task({
+            description: _description,
+            completed: false,
+            owner: msg.sender
+        }));
+    }
+
+    function completeTask(uint _index) public {
+        require(tasks[_index].owner == msg.sender, "Only the owner can complete the task");
+        tasks[_index].completed = true;
+    }
+
+    function getTasks() public view returns (Task[] memory) {
+        return tasks;
+    }
+}
